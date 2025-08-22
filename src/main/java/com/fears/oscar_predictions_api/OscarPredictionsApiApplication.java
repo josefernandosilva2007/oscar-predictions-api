e5034a33d0bd60454cce406fd5c86ad7d3f40903
josefernando <josefernandosilva0501@gmail.com>
2025-08-22 12:44:53 -0300

    first commit

---
+++ src/main/java/com/fears/oscar_predictions_api/OscarPredictionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.fears.oscar_predictions_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscarPredictionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscarPredictionsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fears/oscar_predictions_api/OscarPredictionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fears.oscar_predictions_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscarPredictionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
